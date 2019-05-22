5da246866e17f7105838c35a46454a4d742e8d80
Prashant Madkar <sprashantmadkar@gmail.com>
2019-05-22 13:32:47 +0530

    Learning to scrape amazon asin using cheerio

--- learn.js
+++ learn.js
@@ -1,7 +1,10 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-const html = `
+const html = fs.readFileSync('./amazonIndex.html', { encoding: 'utf8' });
+
+/* const html = `
 <div id="root">
   <ul id="fruits">
     <li class="apple">Apple</li>
@@ -20,7 +23,7 @@ const html = `
     <li class="grape">Grape</li>
   </ul>
 </div>
-`;
+`; */
 
 function testHtml() {
   const $ = cheerio.load(html);
@@ -29,4 +32,37 @@ function testHtml() {
   console.log($('.orange').closest('.apple').text());
 }
 
-testHtml();
+function amazonScraping(){
+  const $ = cheerio.load(html);
+
+/* console.log($('#fruits > li').text());
+console.log($('.orange').closest('.apple').text()); */
+console.log('Product Title = '+$('#productTitle').text().replace(/\s\s+/g,''));
+
+console.log('Product variation = '+$('#a-autoid-13').text().replace(/\s\s+/g,''));
+
+console.log('Product variations = '+$('#a-autoid-14').text().replace(/\s\s+/g,''));
+
+console.log('Product variations = '+$('#a-autoid-15').text().replace(/\s\s+/g,''));
+
+console.log('Product variations = '+$('#a-autoid-12').text().replace(/\s\s+/g,''));
+console.log('Product variations = '+$('#a-autoid-17').text().replace(/\s\s+/g,''));
+console.log('Product Price = '+$('#priceblock_ourprice').text().replace(/\s\s+/g,''));
+
+console.log('Product Brand Name = '+$('#bylineInfo').text().replace(/\s\s+/g,''));
+console.log('Product M.R.P = '+$('#mbc-price-2').text().replace(/\s\s+/g,''));
+
+console.log('Seller rank = '+$('#SalesRank').text().replace(/\s\s+/g,''));
+$('#feature-bullets').each((i,el) =>{
+  const productFeatures = $(el).text().replace(/\s\s+/g,'');
+  console.log('\nProduct Features ='+'\n'+productFeatures);
+})
+
+$('#histogramTable').each((i,el) =>{
+  const ratingsTable = $(el).text().replace(/\s\s+/g,'');
+  console.log("\nProduct Ratings ="+ratingsTable);
+})
+console.log('\nProduct Seller Name = '+$('#merchant-info').text().replace(/\s\s+/g,''));
+}
+amazonScraping();
+//testHtml();
