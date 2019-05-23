011d958631654106f6e69e33b68afd22e0892186
Mahi22 <mahesh.masalkar22@gmail.com>
2019-05-23 15:31:42 +0530

    Learned basic amazom html page scrape

--- learn.js
+++ learn.js
@@ -71,13 +71,68 @@ function amazonScrapingWithParser() {
   const result = parser(html, {
     productTitle: '#productTitle',
     size: '#variation_size_name .selection',
-    ratings: '#acrCustomerReviewText',
+    reviews: '#acrCustomerReviewText',
     mrp: '#price .priceBlockStrikePriceString',
     buyBox: '#sellerProfileTriggerId',
+    features: {
+      selector: '#feature-bullets .a-unordered-list',
+      replace: /\s\s+/g
+    },
+    productDescription: {
+      selector: '#productDescription',
+      replace: /\s\s+/g
+    },
     variants: {
       listItem: '#variation_size_name .a-size-base'
+    },
+    prodDetails: {
+      listItem: '#prodDetails table tbody tr',
+      data: {
+        label: '.label',
+        value: {
+          selector: '.value',
+          replace: {
+            regex: /\s\s+/g,
+            replacement: ' '
+          }
+        }
+      }
+    },
+    ratings: {
+      selector: '#averageCustomerReviews',
+      data: {
+        total: '#acrPopover',
+      }
+    },
+    ratingsTable: {
+      listItem: 'table#histogramTable tbody tr',
+      data: {
+        label: {
+          selector: 'td',
+          eq: 0
+        },
+        value: {
+          selector: 'td',
+          eq: 2
+        }
+      }
+    },
+    featureRatings: {
+      listItem: '#cr-dp-summarization-attributes > #cr-summarization-attributes-list > div > div > div',
+      data: {
+        label: {
+          selector: 'span',
+          texteq: 0
+        },
+        value: {
+          selector: 'span',
+          texteq: 1
+        }
+      }
     }
   });
+
+  console.log(result);
 }
 
 amazonScrapingWithParser();
