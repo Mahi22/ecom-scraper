c46b58df9658df889a023fbf37d1f56db9a18940
Prashant Madkar <sprashantmadkar@gmail.com>
2019-06-02 13:34:48 +0530

    temporary branch for code correction

--- sample.js
+++ sample.js
@@ -1,16 +1,17 @@
 const test = require('ava');
 const dirtree = require('../lib/directoryTree');
-const testTree  = require('./fixture.js');
+const testTree = require('./excludeDir.js');
 const excludeTree = require('./excludeDir.js');
 	test('should return an Object', t => {
-    const tree = dirtree('./test/test_data',{extensions:/\.txt$/});
+    const tree = dirtree('./test_data',{extensions:/\.txt$/});
     t.truthy(typeof tree === 'object');
-    console.log(tree);
+    console.log(tree.children);
   });
 
+
   test('should list the children in a directory', t => {
-    const tree = dirtree('./test/test_data', {extensions:/\.txt$/});
-        t.is(tree.children.length, 4);
+    const tree = dirtree('./test_data', {extensions:/\.txt$/});
+        t.is(tree.children.length, 6);
     // 4 including the empty `some_dir_2`.
   });
 
@@ -38,7 +39,7 @@ const excludeTree = require('./excludeDir.js');
   });
 
   test('should execute a callback function for each file with specified extensions', t => {
-    let number_of_files =  6;
+    let number_of_files =  7;
     let callback_executed_times = 0;
 
     const tree = dirtree('./test/test_data', {extensions:/\.txt$/}, function(item, PATH) {
@@ -51,25 +52,50 @@ const excludeTree = require('./excludeDir.js');
   test('should display the size of a directory (summing up the children)', t => {
     const tree = dirtree('./test/test_data', {extensions:/\.txt$/});
     t.falsy(tree.size>11000);
-    console.log(tree.length);
   });
 
   test('should not crash with directories where the user does not have necessary permissions', t => {
-    const tree = dirtree('/root/', {extensions:/\.txt$/});
-    t.is(tree,null);
-    console.log(tree);
-  });
+	 	const tree = dirtree('/root/', {extensions:/\.txt$/});
+	 	t.is(tree,null);
+	 });
 
-  test('should return the correct exact result', t => {
+    test('should return the correct exact result', t => {
 		const tree = dirtree('./test/test_data', {normalizePath: true});
         t.is(tree,testTree);
         console.log('*****');
         console.log(testTree);
         console.log(tree);
-  }); //not getting
+    }); //not getting
 
-  test('should exclude the correct folders', t => {
-      const tree = dirtree('./test/test_data',{exclude: /some_dir_2/, normalizePath: true});
-      t.deepEqual(tree,excludeTree);
-      console.log(tree);
-  });
+    test('should not swallow exceptions thrown in the callback function', t => {
+		const error = new Error('Something happened!');
+		const badFunction = function () {
+			dirtree('./test/test_data', {extensions:/\.txt$/}, function(item) {
+			  throw error;
+			});
+		}
+		t.is(badFunction,error.message);
+    })
+
+    test('should exclude the correct folders', t => {
+		const tree = dirtree('./test/test_data',{exclude: /another_dir/, normalizePath: true});
+		t.deepEqual(tree,excludeTree);
+	});
+    /* const fn = () => {
+        throw new TypeError('Something happened');
+    };
+    test('throws', t => {
+        const error = t.throws(() => {
+            fn();
+        }, TypeError);
+        t.is(error.message, );
+    }); */
+
+    /* test('should include attributes', t => {
+		const tree = dirtree('./test/test_data',{ attributes: ['mtime', 'ctime']});
+		tree.children.forEach((child) => {
+			if(child.type == 'file'){
+                t.deepEqual();
+			}
+		})
+	}); */
