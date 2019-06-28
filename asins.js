4bc7ddc00dcb11d4a085b0b620ea2b22d3ea0b65
Prashant Sahay <admin@admins-MacBook-Pro.local>
2019-06-28 17:15:20 +0530

    updated the day first available code

--- asins.js
+++ asins.js
@@ -1,12 +1,19 @@
 const { from } = require('rxjs');
-const { concatMap, map } = require('rxjs/operators');
+const { concatMap, map, toArray, tap } = require('rxjs/operators');
 const request = require('request');
 const parser = require('./lib/parser');
 
 const asins = ['B00MIYM0VS',
-    'B00JDACK3S'];
-
+    'B00JDACK3S',
+    'B01MA1JLOM',
+    'B01LYBZX6Y',
+    'B00Y6EHHQ6',
+    'B00N3L0T90',
+    'B00N3L0Z9E',
+    'B00O52UQDA'];
+var counter = 0
 const requestAmazon = asin => from(new Promise((resolve, reject)  => {
+    console.log(`Scraping ${++counter} of ${asins.length}`)
     request(`https://amazon.in/dp/${asin}`, function (err, response, body) {
         if (err){reject(err)}
 
@@ -21,10 +28,13 @@ const amazonRobot$ = from(asins)
 
 const parse$ = amazonRobot$.pipe(map(html => {
     return parser(html, {
-        dateFirst: {
+        dateFirstGlobal: {
             listItem: '.bucket .content ul li',
             filter: x => x.includes('Date')
+        },
+        dateFirstIndia: {
+            selector: '.date-first-available .value'
         }
-    })}));
+    })}), map(obj => ({ dateFirst: obj.dateFirstGlobal.length === 0 ? obj.dateFirstIndia : obj.dateFirstGlobal[0].split(':')[1], belongs: obj.dateFirstGlobal.length === 0 ? 'India' : 'Outside'})), tap(console.log), toArray());
 
 parse$.subscribe(console.log);
